a1b5cdd082e0de3be79bc4ea98cfb91874840bcf
Joshua Kraft <joshuakraft@icloud.com>
2024-02-09 15:22:40 -0600

    remove add place view, collapse functionality into detail view

--- DontGoThere/Views/DetailView.swift
+++ DontGoThere/Views/DetailView.swift
@@ -27,12 +27,51 @@ struct DetailView: View {
     }
   }
   
+  struct CheckboxToggleStyle: ToggleStyle {
+    func makeBody(configuration: Configuration) -> some View {
+      Button(action: {
+        configuration.isOn.toggle()
+      }, label: {
+        HStack {
+          Image(systemName: configuration.isOn ? "checkmark.square" : "square")
+          configuration.label
+        }
+      })
+      .buttonStyle(.plain)
+    }
+  }
+
+  struct TimeUnit {
+    enum tUnits { case days, weeks, months, years }
+    var unit: tUnits
+    var expiryInterval: Int {
+      switch unit {
+      case .days:
+        return 1 * 86400
+      case .weeks:
+        return 7 * 86400
+      case .months:
+        return 30 * 86400
+      case .years:
+        return 365 * 86400
+      }
+    }
+  }
+
   @Environment(\.modelContext) var modelContext
   
   @Bindable var place: Place
   
+  @State private var timeUnit = TimeUnit(unit: .months)
+  @State private var shouldAutoCalcExpiry = true
+  @State private var expiryValue = 3
+  @State private var expiryIsInDays = false
+  @State private var expiryIsInWeeks = false
+  @State private var expiryIsInMonths = true
+  @State private var expiryIsInYears = false
+
   var body: some View {
-    ScrollView(.vertical, showsIndicators: false) {
+    ScrollView(.vertical) {
       VStack(alignment: .leading) {
         Map(initialPosition: position(for: place), interactionModes: []) {
           Marker(place.name, coordinate: CLLocationCoordinate2D(latitude: place.latitude, longitude: place.longitude))
@@ -48,7 +87,7 @@ struct DetailView: View {
           Divider()
             .padding(.bottom, 4)
           VStack(alignment: .leading) {
-            HStack(alignment: .center) {
+            HStack {
               TextLabel("NAME:")
               TextField("Place Name", text: $place.name)
                 .textFieldStyle(.roundedBorder)
@@ -67,25 +106,97 @@ struct DetailView: View {
               DatePicker("Added Date", selection: $place.addDate)
                 .disabled(true)
                 .labelsHidden()
-                .frame(maxWidth: .infinity)
             }
             .padding(.bottom, 4)
             HStack {
-              TextLabel("EXPIRES:")
-              DatePicker("Expiry Date", selection: $place.expirationDate)
+              TextLabel("EXPIRES: ")
+              DatePicker("Expires", selection: $place.expirationDate)
                 .labelsHidden()
-                .frame(maxWidth: .infinity)
+                .disabled(shouldAutoCalcExpiry)
+              Toggle(isOn: $shouldAutoCalcExpiry) {
+                Text("Auto")
+              }
+              .toggleStyle(CheckboxToggleStyle())
+              .padding(.leading)
+              .onChange(of: shouldAutoCalcExpiry) { if !shouldAutoCalcExpiry { updateExpiryValue() } }
             }
             .padding(.bottom, 4)
+            
+            if !shouldAutoCalcExpiry {
+              HStack {
+                Text("SET TO EXPIRE IN:")
+                  .font(.subheadline)
+                  .foregroundStyle(.secondary)
+                  .padding(.leading)
+                
+                Picker("Expiry", selection: $expiryValue) {
+                  ForEach(1...100, id: \.self) { value in
+                    Text(String(value))
+                  }
+                }
+                .labelsHidden()
+                .pickerStyle(.wheel)
+                .frame(height: 85)
+                .onChange(of: expiryValue) {
+                  updateExpiryValue()
+                }
+                
+                VStack(alignment: .leading) {
+                  
+                  Toggle(isOn: $expiryIsInDays) {
+                    Text("Days")
+                  }
+                  .toggleStyle(CheckboxToggleStyle())
+                  .onChange(of: expiryIsInDays) {
+                    daysChecked()
+                  }
+                  
+                  Toggle(isOn: $expiryIsInWeeks) {
+                    Text("Weeks")
+                  }
+                  .toggleStyle(CheckboxToggleStyle())
+                  .onChange(of: expiryIsInWeeks) {
+                    weeksChecked()
+                  }
+                  
+                  Toggle(isOn: $expiryIsInMonths) {
+                    Text("Months")
+                  }
+                  .toggleStyle(CheckboxToggleStyle())
+                  .onChange(of: expiryIsInMonths) {
+                    monthsChecked()
+                  }
+                  
+                  Toggle(isOn: $expiryIsInYears) {
+                    Text("Years")
+                  }
+                  .toggleStyle(CheckboxToggleStyle())
+                  .onChange(of: expiryIsInYears) {
+                    yearsChecked()
+                  }
+                }
+                .padding(.trailing)
+              }
+              .padding(.bottom, 4)
+            }
+
           }
           
           Divider()
             .padding(.bottom, 4)
           
-          Text("Images")
-            .font(.title3)
-            .foregroundStyle(.secondary)
-            .padding(.leading)
+          HStack {
+            Text("Images")
+              .font(.title3)
+              .foregroundStyle(.secondary)
+            
+            Spacer()
+            
+            Button("Add Images", systemImage: "photo.badge.plus") {
+              // bring up a photo picker and save the images
+            }
+          }
+          .padding([.leading, .trailing])
           
           ScrollView(.horizontal, showsIndicators: false) {
             HStack {
@@ -125,6 +236,50 @@ struct DetailView: View {
         center: CLLocationCoordinate2D(latitude: place.latitude, longitude: place.longitude),
         span: MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001))
     )
+  }
+  
+  func updateExpiryValue() {
+    place.expirationDate = place.addDate.addingTimeInterval(TimeInterval(expiryValue * timeUnit.expiryInterval))
+  }
+
+  func daysChecked() {
+    if expiryIsInDays {
+      expiryIsInWeeks = false
+      expiryIsInMonths = false
+      expiryIsInYears = false
+      timeUnit.unit = .days
+      updateExpiryValue()
+    }
+  }
+  
+  func weeksChecked() {
+    if expiryIsInWeeks {
+      expiryIsInDays = false
+      expiryIsInMonths = false
+      expiryIsInYears = false
+      timeUnit.unit = .weeks
+      updateExpiryValue()
+    }
+  }
+
+  func monthsChecked() {
+    if expiryIsInMonths {
+      expiryIsInDays = false
+      expiryIsInWeeks = false
+      expiryIsInYears = false
+      timeUnit.unit = .months
+      updateExpiryValue()
+    }
+  }
+
+  func yearsChecked() {
+    if expiryIsInYears {
+      expiryIsInDays = false
+      expiryIsInWeeks = false
+      expiryIsInMonths = false
+      timeUnit.unit = .years
+      updateExpiryValue()
+    }
   }
 }
 
