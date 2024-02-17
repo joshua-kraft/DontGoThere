345ef3f599831fa2953a4ebd646f3ebefb0a1b49
Joshua Kraft <joshuakraft@icloud.com>
2024-02-17 04:34:43 -0600

    flesh out settings view

--- DontGoThere/Views/SettingsView.swift
+++ DontGoThere/Views/SettingsView.swift
@@ -35,11 +35,13 @@ struct SettingsView: View {
   @AppStorage("neverExpirePlaces") var neverExpire = false
   @AppStorage("autoDeleteInterval") var autoDeleteInterval = 0
   @AppStorage("neverDeletePlaces") var neverDelete = false
-  @AppStorage("maxNotificationCount") var maxNotificationCount = 0
+  @AppStorage("maxNotificationCount") var maxNotificationCount = 10
+  @AppStorage("noNotificationLimit") var noNotificationLimit = false
   
   var body: some View {
     NavigationStack {
       Form {
+        
         Section {
           HStack {
             Text("Never Expire:")
@@ -59,13 +61,50 @@ struct SettingsView: View {
           SettingsHeader(headerTitle: "Expiration Settings", headerNote: "Set the default times to archive places from your active list.")
         }
         
+        
+        
         Section {
+          HStack {
+            Text("Never Delete")
+              .font(neverDelete ? .subheadline.bold() : .subheadline)
+            Spacer()
+            Toggle("Never Delete:", isOn: $neverDelete)
+              .labelsHidden()
+          }
           
+          if !neverDelete {
+            TimeValuePickerView(timeIntervalValue: $autoDeleteInterval, labelText: "SET TO DELETE IN:", pickerTitle: "Deletion Time")
+              .listRowInsets(EdgeInsets())
+              .padding([.top, .bottom])
+          }
         } header: {
           SettingsHeader(headerTitle: "Deletion Settings", headerNote: "Set the default time before archived places are automatically deleted.")
         }
         
+        
         Section {
+          HStack {
+            Text("No Limit")
+              .font(noNotificationLimit ? .subheadline.bold() : .subheadline)
+            Spacer()
+            Toggle("No Limit", isOn: $noNotificationLimit)
+              .labelsHidden()
+          }
+          
+          if !noNotificationLimit {
+            HStack {
+              Text("NOTIFICATION LIMIT:")
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+              Picker("Notification Limit", selection: $maxNotificationCount) {
+                ForEach(1...100, id: \.self) { value in
+                  Text(String(value))
+                }
+              }
+              .pickerStyle(.wheel)
+              .frame(height: 85)
+            }
+          }
         } header: {
           SettingsHeader(headerTitle: "Notification Settings", headerNote: "Set the max amount of times DontGoThere will remind you not to go to a place before archiving the place automatically.")
         }
