642bf2ce8ed6524dd24a466082203b65c5598757
Joshua Kraft <joshuakraft@icloud.com>
2024-03-01 12:47:21 -0600

    add map controls in zstack to map

--- DontGoThere/PlaceMap/PlacesMapView.swift
+++ DontGoThere/PlaceMap/PlacesMapView.swift
@@ -26,6 +26,7 @@ struct PlacesMapView: View {
   
   @State private var position: MapCameraPosition = .userLocation(fallback: .automatic)
   @State private var visibleRegion: MKCoordinateRegion?
+  @Namespace var mapScope
   
   let notSearchingOverlayText = "Tap on the map to add a place at that location. Tap and hold on a place to view details, share, or delete."
   let searchingOverlayText = "Tap on a seach result or marker to add a place at that location. End searching to show your current places."
@@ -34,51 +35,61 @@ struct PlacesMapView: View {
     NavigationStack(path: $path) {
       VStack {
         MapReader { proxy in
-          Map(position: $position) {
-            if showExistingPlaces {
-              ForEach(places.filter { !$0.isArchived }) { place in
-                Annotation(place.name, coordinate: place.coordinate) {
-                  DontGoThereIconView(width: 40, height: 32)
-                    .contextMenu {
-                      Button("Show Details", systemImage: "list.dash") { path.append(place) }
-                      Button("Delete", systemImage: "trash", role: .destructive) {
-                        deletedPlace = place
-                        isShowingDeleteAlert = true
+          ZStack(alignment: .bottomTrailing) {
+            Map(position: $position, scope: mapScope) {
+              if showExistingPlaces {
+                ForEach(places.filter { !$0.isArchived }) { place in
+                  Annotation(place.name, coordinate: place.coordinate) {
+                    DontGoThereIconView(width: 40, height: 32)
+                      .contextMenu {
+                        Button("Show Details", systemImage: "list.dash") { path.append(place) }
+                        Button("Delete", systemImage: "trash", role: .destructive) {
+                          deletedPlace = place
+                          isShowingDeleteAlert = true
+                        }
                       }
+                  }
+                }
+              }
+              
+              ForEach(searchResults) { result in
+                Annotation("Name", coordinate: result.coordinate) {
+                  DontGoThereSearchIconView(width: 50, height: 50)
+                    .tag(result)
+                    .onTapGesture {
+                      withAnimation {
+                        isShowingSearchSheet = false
+                      }
+                      addPlace(at: result.coordinate)
                     }
                 }
               }
             }
-            
-            ForEach(searchResults) { result in
-              Annotation("Name", coordinate: result.coordinate) {
-                DontGoThereSearchIconView(width: 50, height: 50)
-                  .tag(result)
-                  .onTapGesture {
-                    withAnimation {
-                      isShowingSearchSheet = false
-                    }
-                    addPlace(at: result.coordinate)
-                  }
+            .onTapGesture { position in
+              if let tappedCoordinate = proxy.convert(position, from: .local) {
+                if searchResults.isEmpty {
+                  addPlace(at: tappedCoordinate)
+                }
               }
             }
-            
-          }
-          .onTapGesture { position in
-            if let tappedCoordinate = proxy.convert(position, from: .local) {
-              if searchResults.isEmpty {
-                addPlace(at: tappedCoordinate)
-              }
+            .safeAreaInset(edge: .top) {
+              Text(isShowingSearchSheet ? searchingOverlayText : notSearchingOverlayText)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .font(.subheadline.bold())
+                .background(.thinMaterial.opacity(0.8))
+                .multilineTextAlignment(.center)
             }
+            
+            VStack {
+              MapUserLocationButton(scope: mapScope)
+              MapCompass(scope: mapScope)
+                .mapControlVisibility(.visible)
+            }
+            .padding(.trailing, 10)
+            .buttonBorderShape(.circle)
           }
-        }
-        .safeAreaInset(edge: .top) {
-          Text(isShowingSearchSheet ? searchingOverlayText : notSearchingOverlayText)
-            .frame(maxWidth: .infinity)
-            .padding()
-            .font(.subheadline.bold())
-            .background(.thinMaterial.opacity(0.8))
-            .multilineTextAlignment(.center)
+          .mapScope(mapScope)
         }
       }
       .navigationTitle("PlaceMap")
