8ad8390365b052daca9cd3e55286fdda92af968a
Joshua Kraft <joshuakraft@icloud.com>
2024-03-04 12:38:10 -0600

    add alerts if user chooses a location auth that isnt what i want, point to settings or allow user to keep choice, only display alerts once

--- DontGoThere/ContentView.swift
+++ DontGoThere/ContentView.swift
@@ -10,8 +10,18 @@ import SwiftUI
 
 struct ContentView: View {
   
+  @Environment(\.openURL) var openURL
+  
   @StateObject var appSettings = AppSettings.loadSettings()
   @StateObject var locationServicesController = LocationServicesController()
+  
+  @State private var isShowingLocationServicesDeniedAlert = false
+  @State private var isShowingLocationServicesAllowedInUseAlert = false
+  @State private var isShowingLocationServicesRestrictedAlert = false
+  
+  @AppStorage("locationServicesDeniedAlertCount") private var locationServicesDeniedAlertCount = 0
+  @AppStorage("locationServicesAllowedInUseOnlyAlertCount") private var locationServicesAllowedInUseOnlyAlertCount = 0
+  @AppStorage("locationServicesRestrictedAlertCount") private var locationServicesRestrictedAlertCount = 0
   
   var body: some View {
     TabView {
@@ -35,6 +45,47 @@ struct ContentView: View {
     .onAppear {
       locationServicesController.checkLocationAuth()
     }
+    .onReceive(NotificationCenter.default.publisher(for: .locationPermissionsDenied)) { _ in
+      if locationServicesDeniedAlertCount == 0 {
+        locationServicesDeniedAlertCount += 1
+        isShowingLocationServicesDeniedAlert.toggle()
+      }
+    }
+    .alert("Location Services Denied", isPresented: $isShowingLocationServicesDeniedAlert) {
+      Button("Settings") {
+        openURL(URL(string: UIApplication.openSettingsURLString)!)
+      }
+      Button("Keep Denied", role: .cancel) { }
+    } message: {
+      Text("Denying location services means that DontGoThere won't be able to add places at your current location, show your location to you on the PlaceMap, or notify you if you approach one of your places.")
+    }
+    .onReceive(NotificationCenter.default.publisher(for: .locationPermissionAuthorizedWhenInUse)) { _ in
+      if locationServicesAllowedInUseOnlyAlertCount == 0 {
+        locationServicesAllowedInUseOnlyAlertCount += 1
+        isShowingLocationServicesAllowedInUseAlert.toggle()
+      }
+    }
+    .alert("Location Services Only Authorized While In Use", isPresented: $isShowingLocationServicesAllowedInUseAlert) {
+      Button("Settings") {
+        openURL(URL(string: UIApplication.openSettingsURLString)!)
+      }
+      Button("Keep Only In Use", role: .cancel) { }
+    } message: {
+      Text("Only allowing location services while in use means that DontGoThere won't be able to notify you as you approach a place.")
+    }
+    .onReceive(NotificationCenter.default.publisher(for: .locationPermissionsRestricted)) { _ in
+      if locationServicesRestrictedAlertCount == 0 {
+        locationServicesRestrictedAlertCount += 1
+        isShowingLocationServicesRestrictedAlert.toggle()
+      }
+    }
+    .alert("Location Services Restricted", isPresented: $isShowingLocationServicesRestrictedAlert) {
+      Button("OK", role: .cancel) { }
+    } message: {
+      Text("Your location services are restricted on this device, likely due to parental controls. Contact the manager of your device to enable location services for DontGoThere.")
+    }
+
+
   }
 }
 
