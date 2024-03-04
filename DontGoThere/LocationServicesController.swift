b76136c5266358e1f533f10d2032267fd449942f
Joshua Kraft <joshuakraft@icloud.com>
2024-03-04 11:47:40 -0600

    post notifications to show alerts if the location authorization status isn't what i need comments for clarity helper function to check authorization status for UI element displaying/hiding

--- DontGoThere/LocationServicesController.swift
+++ DontGoThere/LocationServicesController.swift
@@ -10,9 +10,16 @@ import Foundation
 import MapKit
 import SwiftUI
 
+extension Notification.Name {
+  static let locationPermissionsDenied = Notification.Name("locationPermissionsDenied")
+  static let locationPermissionAuthorizedWhenInUse = Notification.Name("locationPermissionAuthorizedWhenInUse")
+  static let locationPermissionsRestricted = Notification.Name("locationPermissionsRestricted")
+}
+
 class LocationServicesController: NSObject, CLLocationManagerDelegate, ObservableObject {
   
   var locationManager: CLLocationManager?
+  let allowedLocationAuths: [CLAuthorizationStatus] = [.authorizedAlways, .authorizedWhenInUse]
   
   func checkLocationAuth() {
     guard let locationManager = locationManager else {
@@ -25,24 +32,34 @@ class LocationServicesController: NSObject, CLLocationManagerDelegate, Observabl
       return
     }
     
+    // request location authorization or post a notification so the user can be prompted to fix it or at least be aware of the effects
     switch locationManager.authorizationStatus {
     case .notDetermined:
       locationManager.requestAlwaysAuthorization()
     case .restricted:
-      print("Location Services are restricted.")
+      NotificationCenter.default.post(name: .locationPermissionsRestricted, object: nil)
     case .denied:
-      print("Location Services are denied.")
-    case .authorizedAlways: 
-      print("Location Services are always authorized.")
+      NotificationCenter.default.post(name: .locationPermissionsDenied, object: nil)
+    case .authorizedAlways:
+      // this is the status we want - we don't need to do anything else
+      break
     case .authorizedWhenInUse:
-      print("Location Services authorized when in use.")
+      NotificationCenter.default.post(name: .locationPermissionAuthorizedWhenInUse, object: nil)
     case .authorized:
-      print("Location Services authorized for single use.")
+      // this is a deprecated status replaced by authorizedAlways
+      break
     @unknown default:
       // Could be reached if Apple adds values to CLAuthorizationStatus
       break
     }
     
+  }
+  
+  func locationIsAuthorized() -> Bool {
+    guard let locationManager else {
+      return false
+    }
+    return allowedLocationAuths.contains(locationManager.authorizationStatus)
   }
   
   func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
