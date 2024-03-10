2d6a670408931651a7f144cb76e08181c8818ebe
Joshua Kraft <joshuakraft@icloud.com>
2024-03-10 04:45:19 -0500

    create separate notification section for edit view

---
+++ DontGoThere/Editing/NotificationSectionView.swift
@@ -0,0 +1,74 @@
+//
+//  NotificationSectionView.swift
+//  DontGoThere
+//
+//  Created by Joshua Kraft on 3/10/24.
+//
+
+import SwiftUI
+
+struct NotificationSectionView: View {
+
+  @Bindable var place: Place
+  @EnvironmentObject var appSettings: AppSettings
+  @FocusState private var focusedField: Field?
+
+  enum Field {
+    case distanceField
+  }
+
+  var body: some View {
+    VStack(alignment: .leading) {
+      HeaderLabel("Notifications")
+        .padding(.leading)
+
+      HStack {
+        DetailLabel("Notification Distance:")
+          .padding([.leading])
+
+        Spacer()
+
+        TextField("NotificationDistance", value: .constant(50), format: .number)
+          .multilineTextAlignment(.trailing)
+          .textFieldStyle(.roundedBorder)
+          .keyboardType(.numberPad)
+          .frame(width: 75)
+          .padding(.trailing, 6)
+          .focused($focusedField, equals: .distanceField)
+          .onChange(of: focusedField) {
+            if focusedField == .distanceField {
+              Task {
+                UIApplication.shared.sendAction(#selector(UIResponder.selectAll(_:)), to: nil, from: nil, for: nil)
+              }
+            }
+          }
+
+        Text("Meters")
+          .padding([.trailing])
+      }
+      .padding(.bottom, 4)
+
+      HStack {
+        DetailLabel("Max Notification Count:")
+          .padding([.leading])
+        Spacer()
+        Text("25")
+          .padding([.trailing])
+        Stepper("Notification Count", value: .constant(25), in: 1...30)
+          .labelsHidden()
+      }
+      .padding(.bottom, 4)
+    }
+  }
+}
+
+#Preview {
+  do {
+    let previewer = try Previewer()
+    return NotificationSectionView(place: previewer.activePlace)
+      .modelContainer(previewer.container)
+      .environmentObject(AppSettings.defaultSettings)
+  } catch {
+    return Text("Failed to create preview: \(error.localizedDescription)")
+  }
+}
