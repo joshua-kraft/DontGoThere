613061129a76bee4d56eed4a455169ea42b23eb7
Joshua Kraft <joshuakraft@icloud.com>
2024-03-30 05:14:27 -0500

    basic onboarding finished view

--- DontGoThere/Views/Onboarding/OnboardingFinished.swift
+++ DontGoThere/Views/Onboarding/OnboardingFinished.swift
@@ -8,11 +8,56 @@
 import SwiftUI
 
 struct OnboardingFinished: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+  @Binding var phase: OnboardingPhase
+  @AppStorage("onboardingComplete") private var onboardingComplete: Bool = false
+
+  var body: some View {
+    VStack {
+      HStack {
+        Button("Back") {
+          withAnimation {
+            phase = .notificationPermission
+          }
+        }
+        .modifier(OnboardingBackButtonModifier())
+        Spacer()
+      }
+
+      Text("You're all set!")
+        .modifier(OnboardingTitleModifier(bigger: true))
+
+      Spacer()
+
+      Text("Shortly you'll be able to add your first place.")
+        .modifier(OnboardingTextModifier())
+
+      Text("Places have a name, location, address, expiration date, review, and photos.")
+        .modifier(OnboardingTextModifier())
+
+      Text("When you add a place, you can configure the notification radius and the maximum notifications allowed.")
+        .modifier(OnboardingTextModifier())
+
+      Text("Changes to the map location of a place will update the address, and vice versa.")
+        .modifier(OnboardingTextModifier())
+
+      Text("Add a place at your current location with the Add Button (+), tap anywhere on the PlaceMap, or search for a place to add.")
+        .modifier(OnboardingTextModifier())
+
+      Spacer()
+
+      Button("Finished!") {
+        withAnimation {
+          onboardingComplete = true
+        }
+      }
+      .modifier(OnboardingForwardButtonModifier())
     }
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .background(OnboardingBackgroundGradient())
+  }
 }
 
 #Preview {
-    OnboardingFinished()
+  OnboardingFinished(phase: .constant(.finished))
 }
