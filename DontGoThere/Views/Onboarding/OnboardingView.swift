c6902514ee2d802316f7aaa684b30e21abd7fc15
Joshua Kraft <joshuakraft@icloud.com>
2024-03-30 00:16:30 -0500

    reorganization in prep for onboarding

---
+++ DontGoThere/Views/Onboarding/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  DontGoThere
+//
+//  Created by Joshua Kraft on 3/30/24.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
