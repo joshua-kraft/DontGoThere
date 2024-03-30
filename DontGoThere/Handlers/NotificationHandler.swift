bdd1d4bef8ad836dfa1bfed048737ebc06b3ebe9
Joshua Kraft <joshuakraft@icloud.com>
2024-03-29 19:06:22 -0500

    basic notification logic

--- DontGoThere/Handlers/NotificationHandler.swift
+++ DontGoThere/Handlers/NotificationHandler.swift
@@ -13,9 +13,27 @@ class NotificationHandler: ObservableObject {
 
   static let shared = NotificationHandler()
 
+  let calendar = Calendar.autoupdatingCurrent
+
   func sendNotification(for place: Place, with event: CLMonitor.Event) {
-    let eventTime = event.date.description
-    print("firing a notification for \(place.name) at \(event.date.description)")
+    guard !calendar.isDateInToday(place.addDate) else {
+      print("Not sending a notification because place was added today")
+      return
+    }
+
+    guard !calendar.isDateInToday(place.lastNotificationDate) else {
+      print("Not sending a notification because place has had a notification sent today")
+      return
+    }
+
+    // If we got here, schedule an immediate notification.
+    print("theoretically sent a notification")
+
+    // Then set the last notification date.
+    place.lastNotificationDate = event.date
+    print("notification date set to \(event.date)")
+
+    print("ending notification function")
   }
 
 }
