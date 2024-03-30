d837324bfdd50fd911b2abc0aaa273af2b64e64e
Joshua Kraft <joshuakraft@icloud.com>
2024-03-30 04:32:14 -0500

    add permission screen stub

---
+++ DontGoThere/Views/Onboarding/OnboardingNotificationPermissionView.swift
@@ -0,0 +1,22 @@
+//
+//  OnboardingNotificationPermissionView.swift
+//  DontGoThere
+//
+//  Created by Joshua Kraft on 3/30/24.
+//
+
+import SwiftUI
+
+struct OnboardingNotificationPermissionView: View {
+
+  @Binding var phase: OnboardingPhase
+  @State private var didRequestNotificationAuth = false
+
+  var body: some View {
+    
+  }
+}
+
+#Preview {
+  OnboardingNotificationPermissionView()
+}
