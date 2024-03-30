a303c0fa5924434dd9adeccb49911dc1f58d46da
Joshua Kraft <joshuakraft@icloud.com>
2024-03-30 01:03:51 -0500

    background color and title text

--- DontGoThere/Views/Onboarding/OnboardingView.swift
+++ DontGoThere/Views/Onboarding/OnboardingView.swift
@@ -8,11 +8,26 @@
 import SwiftUI
 
 struct OnboardingView: View {
-
-  
-
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    NavigationStack {
+      VStack {
+        Text("DontGoThere")
+          .font(.largeTitle.bold())
+          .foregroundStyle(.white.gradient)
+
+        Spacer()
+      }
+      .navigationBarTitleDisplayMode(.inline)
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
+      .background {
+        LinearGradient(stops: [
+          .init(color: .darkenedLaunch, location: 0.20),
+          .init(color: .launch, location: 0.80),
+          .init(color: .darkenedLaunch, location: 1.0)
+        ], startPoint: .top, endPoint: .bottom)
+        .ignoresSafeArea()
+      }
+    }
   }
 }
 
