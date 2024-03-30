b764de58ff78b5c2bd688914c7aec594c801347f
Joshua Kraft <joshuakraft@icloud.com>
2024-03-30 00:11:53 -0500

    move alerts out of content view in prep for onboarding screen

--- DontGoThere/ContentView.swift
+++ DontGoThere/ContentView.swift
@@ -18,13 +18,6 @@ struct ContentView: View {
 
   @Environment(\.openURL) var openURL
 
-  @State private var showingLocationDeniedAlert = false
-  @State private var showingLocationAllowedInUseOnlyAlert = false
-  @State private var showingLocationRestrictedAlert = false
-  @AppStorage("locationDeniedAlertCount") private var locationDeniedAlertCount = 0
-  @AppStorage("locationAllowedOnlyInUseAlertCount") private var locationAllowedOnlyInUseAlertCount = 0
-  @AppStorage("locationRestrictedAlertCount") private var locationRestrictedAlertCount = 0
-
   @State private var selectedTab: DontGoThereTabs = .list
 
   var body: some View {
@@ -46,48 +39,6 @@ struct ContentView: View {
           Label("Settings", systemImage: "gearshape.2")
         }
         .tag(DontGoThereTabs.settings)
-    }
-    .onReceive(NotificationCenter.default.publisher(for: .locationPermissionsDenied)) { _ in
-      if locationDeniedAlertCount == 0 {
-        locationDeniedAlertCount += 1
-        showingLocationDeniedAlert.toggle()
-      }
-    }
-    .alert("Location Services Denied", isPresented: $showingLocationDeniedAlert) {
-      Button("Settings") {
-        openURL(URL(string: UIApplication.openSettingsURLString)!)
-      }
-      Button("Keep Denied", role: .cancel) { }
-    } message: {
-      Text("Denying location services will reduce the intended functionality of DontGoThere. ")
-      + Text("DontGoThere wont be able to show or add places at your current location. ")
-      + Text("Additionally, DontGoThere won't be able to send you notifications not to go to a place.")
-    }
-    .onReceive(NotificationCenter.default.publisher(for: .locationPermissionsAuthorizedWhenInUse)) { _ in
-      if locationAllowedOnlyInUseAlertCount == 0 {
-        locationAllowedOnlyInUseAlertCount += 1
-        showingLocationAllowedInUseOnlyAlert.toggle()
-      }
-    }
-    .alert("Location Only Authorized While In Use", isPresented: $showingLocationAllowedInUseOnlyAlert) {
-      Button("Settings") {
-        openURL(URL(string: UIApplication.openSettingsURLString)!)
-      }
-      Button("Keep Only In Use", role: .cancel) { }
-    } message: {
-      Text("This means that DontGoThere won't be able to notify you as you approach a place.")
-    }
-    .onReceive(NotificationCenter.default.publisher(for: .locationPermissionsRestricted)) { _ in
-      if locationRestrictedAlertCount == 0 {
-        locationRestrictedAlertCount += 1
-        showingLocationRestrictedAlert.toggle()
-      }
-    }
-    .alert("Location Services Restricted", isPresented: $showingLocationRestrictedAlert) {
-      Button("OK", role: .cancel) { }
-    } message: {
-      Text("Your location services are restricted on this device, likely due to parental controls. ")
-      + Text("Contact the manager of your device to enable location services for DontGoThere.")
     }
     .onReceive(NotificationCenter.default.publisher(for: .dontGoThereNotificationWasOpened)) { _ in
 
