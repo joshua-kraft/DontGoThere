3b8281e26a676d5457b801dcd2234c13b0a07618
Joshua Kraft <joshuakraft@icloud.com>
2024-03-30 04:56:08 -0500

    basic notification permission onboarding screen

--- DontGoThere/Views/Onboarding/OnboardingNotificationPermissionView.swift
+++ DontGoThere/Views/Onboarding/OnboardingNotificationPermissionView.swift
@@ -10,13 +10,64 @@ import SwiftUI
 struct OnboardingNotificationPermissionView: View {
 
   @Binding var phase: OnboardingPhase
-  @State private var didRequestNotificationAuth = false
+  @AppStorage("didRequestNotificationAuth") private var didRequestNotificationAuth = false
 
   var body: some View {
-    
+    VStack {
+      HStack {
+        Button("Back") {
+          withAnimation {
+            phase = .locationPermission
+          }
+        }
+        .modifier(OnboardingBackButtonModifier())
+
+        Spacer()
+      }
+
+      Text("Notification Permission")
+        .modifier(OnboardingTitleModifier())
+
+      Spacer()
+
+      Text("DontGoThere will send you notifications if you get close enough to a place to remind you not to go.")
+        .modifier(OnboardingTextModifier())
+
+      Text("You set the radius to be notified within when you create the place.")
+        .modifier(OnboardingTextModifier())
+
+      Text("It will only send one notification per place visited per day.")
+        .modifier(OnboardingTextModifier())
+
+      Text("You can set the maximum amount of times any one place will generate a notification in the settings.")
+        .modifier(OnboardingTextModifier(smaller: true))
+
+      Text("To do this, DontGoThere needs your permission to send notifications.")
+        .modifier(OnboardingTextModifier(smaller: true))
+
+      Spacer()
+
+      Button("Grant Notification Permission") {
+        NotificationHandler.shared.requestNotificationPermissions()
+        didRequestNotificationAuth = true
+      }
+      .modifier(OnboardingForwardButtonModifier())
+      .disabled(didRequestNotificationAuth)
+
+      Button("Finish") {
+        withAnimation {
+          phase = .finished
+        }
+      }
+      .modifier(OnboardingForwardButtonModifier())
+      .disabled(!didRequestNotificationAuth)
+
+    }
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .background(OnboardingBackgroundGradient())
   }
 }
 
 #Preview {
-  OnboardingNotificationPermissionView()
+  OnboardingNotificationPermissionView(phase: .constant(.notificationPermission))
 }
