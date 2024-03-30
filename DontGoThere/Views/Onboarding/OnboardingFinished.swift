e0bf52a461b5d4c012e0907ae819fb6d521931b9
Joshua Kraft <joshuakraft@icloud.com>
2024-03-30 04:49:00 -0500

    stubbed finish screen

---
+++ DontGoThere/Views/Onboarding/OnboardingFinished.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingFinished.swift
+//  DontGoThere
+//
+//  Created by Joshua Kraft on 3/30/24.
+//
+
+import SwiftUI
+
+struct OnboardingFinished: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingFinished()
+}
