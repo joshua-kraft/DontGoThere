94a080f3186e4ae2b3de5e1ad546d0bdd9c32088
Joshua Kraft <joshuakraft@icloud.com>
2024-03-29 19:52:25 -0500

    implement permissions and notification sending

--- DontGoThere/Handlers/NotificationHandler.swift
+++ DontGoThere/Handlers/NotificationHandler.swift
@@ -8,6 +8,7 @@
 import CoreLocation
 import Foundation
 import SwiftUI
+import UserNotifications
 
 class NotificationHandler: ObservableObject {
 
@@ -26,14 +27,52 @@ class NotificationHandler: ObservableObject {
       return
     }
 
-    // If we got here, schedule an immediate notification.
-    print("theoretically sent a notification")
+    // If we got here, create a notification
 
-    // Then set the last notification date.
-    place.lastNotificationDate = event.date
-    print("notification date set to \(event.date)")
+    let unc = UNUserNotificationCenter.current()
 
-    print("ending notification function")
+    let addNotificationRequest = {
+      let content = UNMutableNotificationContent()
+      content.title = "Going to \(place.name)? DontGoThere!"
+      content.subtitle = "You had a bad experience at \(place.name) on \(place.formattedAddDate)."
+      content.sound = UNNotificationSound.default
+      let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+      let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+      unc.add(request)
+    }
+
+    unc.getNotificationSettings { notificationSettings in
+      if notificationSettings.authorizationStatus == .authorized {
+        print("Sending a notification")
+        place.lastNotificationDate = event.date
+        place.notificationCount += 1
+        print("notification date set to \(event.date)")
+        print("notification count for place is: \(place.notificationCount)")
+        addNotificationRequest()
+      } else {
+        print("not sending a notification because we are not authorized to do so")
+      }
+    }
+  }
+
+  func requestNotificationPermissions() {
+    let unc = UNUserNotificationCenter.current()
+
+    unc.getNotificationSettings { notificationSettings in
+      if notificationSettings.authorizationStatus == .notDetermined {
+        unc.requestAuthorization(options: [.alert, .sound]) { success, error in
+          if success {
+            print("notification settings granted")
+          } else if let error {
+            print("ran into an error while requesting permissions")
+            print(error.localizedDescription)
+          }
+        }
+      } else {
+        // do nothing. Either they have denied permissions or we have been granted, which is handled when sending notifications
+      }
+    }
+
   }
 
 }
