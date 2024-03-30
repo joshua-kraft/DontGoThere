34bd26afd4faa39273d5800517151ed3d2c9092c
Joshua Kraft <joshuakraft@icloud.com>
2024-03-30 04:31:42 -0500

    remove navigation stack from onboarding

--- DontGoThere/Views/Onboarding/OnboardingLocationPermissionsView.swift
+++ DontGoThere/Views/Onboarding/OnboardingLocationPermissionsView.swift
@@ -13,58 +13,55 @@ struct OnboardingLocationPermissionsView: View {
   @State private var didRequestLocationAuth = false
 
   var body: some View {
-    NavigationStack {
-      VStack {
-
-        HStack {
-          Button("Back") {
-            withAnimation {
-              phase = .intro
-            }
-          }
-          .modifier(OnboardingBackButtonModifier())
-
-          Spacer()
-        }
-
-        Text("Location Permissions")
-          .modifier(OnboardingTitleModifier())
-
-        Spacer()
-
-        Text("DontGoThere shows you your current location on the PlaceMap.")
-          .modifier(OnboardingTextModifier())
-
-        Text("When you add a new place, it is added at your current location by default.")
-          .modifier(OnboardingTextModifier())
-
-        Text("To do this, DontGoThere needs permission to access your location data through Location Services.")
-          .modifier(OnboardingTextModifier())
-
-        Text("DontGoThere never keeps any data related to your location.")
-          .modifier(OnboardingTextModifier())
-
-        Spacer()
-
-        Button("Grant Location Permission") {
-          LocationHandler.shared.startup()
-          didRequestLocationAuth = true
-        }
-        .modifier(OnboardingForwardButtonModifier())
-        .disabled(didRequestLocationAuth)
-
-        Button("Next: Notification Permission") {
+    VStack {
+      HStack {
+        Button("Back") {
           withAnimation {
-            phase = .notificationPermission
+            phase = .intro
           }
         }
-        .modifier(OnboardingForwardButtonModifier())
-        .disabled(!didRequestLocationAuth)
+        .modifier(OnboardingBackButtonModifier())
 
+        Spacer()
       }
-      .frame(maxWidth: .infinity, maxHeight: .infinity)
-      .background(OnboardingBackgroundGradient())
+      
+      Text("Location Permissions")
+        .modifier(OnboardingTitleModifier())
+
+      Spacer()
+
+      Text("DontGoThere shows you your current location on the PlaceMap.")
+        .modifier(OnboardingTextModifier())
+
+      Text("When you add a new place, it is added at your current location by default.")
+        .modifier(OnboardingTextModifier())
+
+      Text("To do this, DontGoThere needs permission to access your location data through Location Services.")
+        .modifier(OnboardingTextModifier())
+
+      Text("DontGoThere never keeps any data related to your location.")
+        .modifier(OnboardingTextModifier())
+
+      Spacer()
+
+      Button("Grant Location Permission") {
+        LocationHandler.shared.startup()
+        didRequestLocationAuth = true
+      }
+      .modifier(OnboardingForwardButtonModifier())
+      .disabled(didRequestLocationAuth)
+
+      Button("Next: Notification Permission") {
+        withAnimation {
+          phase = .notificationPermission
+        }
+      }
+      .modifier(OnboardingForwardButtonModifier())
+      .disabled(!didRequestLocationAuth)
+
     }
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .background(OnboardingBackgroundGradient())
   }
 }
 
