08f77482be6ce28ce9ad4011e644a2740f6f536f
Joshua Kraft <joshuakraft@icloud.com>
2024-03-30 04:10:53 -0500

    add location permissions view

--- DontGoThere/Views/Onboarding/OnboardingLocationPermissionsView.swift
+++ DontGoThere/Views/Onboarding/OnboardingLocationPermissionsView.swift
@@ -8,11 +8,59 @@
 import SwiftUI
 
 struct OnboardingLocationPermissionsView: View {
-    var body: some View {
-        Text("OnboardingLocationPermissionsView")
+
+  @Binding var phase: OnboardingPhase
+
+  var body: some View {
+    NavigationStack {
+      VStack {
+
+        HStack {
+          Button("Back") {
+            withAnimation {
+              phase = .intro
+            }
+          }
+          .modifier(OnboardingBackButtonModifier())
+
+          Spacer()
+        }
+
+        Text("Location Permissions")
+          .modifier(OnboardingTitleModifier())
+
+        Spacer()
+
+        Text("DontGoThere shows you your current location on the PlaceMap.")
+          .modifier(OnboardingTextModifier())
+
+        Text("When you add a new place, it is added at your current location by default.")
+          .modifier(OnboardingTextModifier())
+
+        Text("To do this, DontGoThere needs permission to access your location data through Location Services.")
+          .modifier(OnboardingTextModifier())
+
+        Spacer()
+
+        Button("Grant Location Permission") {
+          LocationHandler.shared.startup()
+        }
+        .modifier(OnboardingForwardButtonModifier())
+
+        Button("Next: Notification Permission") {
+          withAnimation {
+            phase = .notificationPermission
+          }
+        }
+        .modifier(OnboardingForwardButtonModifier())
+
+      }
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
+      .background(OnboardingBackgroundGradient())
     }
+  }
 }
 
 #Preview {
-    OnboardingLocationPermissionsView()
+  OnboardingLocationPermissionsView(phase: .constant(.locationPermission))
 }
