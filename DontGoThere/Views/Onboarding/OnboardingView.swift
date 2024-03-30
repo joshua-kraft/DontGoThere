4c8a65824e48efa5355e26b4870e2d46480eee84
Joshua Kraft <joshuakraft@icloud.com>
2024-03-30 01:54:48 -0500

    basic first onboarding screen

--- DontGoThere/Views/Onboarding/OnboardingView.swift
+++ DontGoThere/Views/Onboarding/OnboardingView.swift
@@ -14,18 +14,70 @@ struct OnboardingView: View {
   var body: some View {
     NavigationStack {
       VStack {
-        Text("DontGoThere")
-          .font(.largeTitle.bold())
-          .foregroundStyle(.white.gradient)
-
-        
+        Text("Welcome to DontGoThere")
+          .modifier(OnboardingTitleModifier())
 
         Spacer()
+
+        Text("DontGoThere helps you keep track of the places you've been where you don't want to be again.")
+          .modifier(OnboardingTextModifier())
+
+        Text("You add places as you have poor experiences. The next time you approach, DontGoThere reminds you not to.")
+          .modifier(OnboardingTextModifier())
+
+        Text("The next screens will go through a series of permission requests for your location and notifications.")
+          .modifier(OnboardingTextModifier())
+
+        Text("DontGoThere does not keep any information about your location or how you use the application.")
+          .modifier(OnboardingTextModifier())
+
+        Spacer()
+
+        OnboardingContinueButton()
+          .onTapGesture {
+            print("Button tapped")
+          }
+
       }
-      .navigationBarTitleDisplayMode(.inline)
       .frame(maxWidth: .infinity, maxHeight: .infinity)
       .background(LaunchBackgroundGradient())
     }
+  }
+}
+
+struct OnboardingContinueButton: View {
+  var body: some View {
+    ZStack {
+      Rectangle()
+        .frame(height: 80)
+        .foregroundStyle(.white)
+        .clipShape(.capsule)
+        .padding()
+
+      Text("Continue")
+        .font(.title2.bold())
+        .foregroundStyle(.launch)
+    }
+  }
+}
+
+struct OnboardingTitleModifier: ViewModifier {
+  func body(content: Content) -> some View {
+    content
+      .multilineTextAlignment(.center)
+      .font(.largeTitle.bold())
+      .foregroundStyle(.white)
+      .padding()
+  }
+}
+
+struct OnboardingTextModifier: ViewModifier {
+  func body(content: Content) -> some View {
+    content
+      .multilineTextAlignment(.center)
+      .foregroundStyle(.white)
+      .font(.title3)
+      .padding()
   }
 }
 
