2a3b02593bf28b71bf800e6b2679e21dc3d7de99
Joshua Kraft <joshuakraft@icloud.com>
2024-09-15 00:10:02 -0500

    create header bar

--- Website/DontGoThereSite/Sources/Components/HeaderBar.swift
+++ Website/DontGoThereSite/Sources/Components/HeaderBar.swift
@@ -1,8 +1,20 @@
 //
-//  File.swift
-//  
+//  HeaderBar.swift
+//
 //
 //  Created by Joshua Kraft on 9/15/24.
 //
 
 import Foundation
+import Ignite
+
+struct HeaderBar: Component {
+  func body(context: PublishingContext) -> [any PageElement] {
+    NavigationBar(logo: "DontGoThere") {
+      Link("Privacy Statement", target: Privacy())
+      Link("Support", target: Support())
+    }
+    .navigationBarStyle(.dark)
+    .background(.royalBlue)
+  }
+}
